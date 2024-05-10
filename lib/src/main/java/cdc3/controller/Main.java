9b4cbfdb367bd810155312250e814655dbbb3aeb
Angel Donet <angdongra@alum.us.es>
2024-05-10 12:05:06 +0200

    CDC3 - control 3

--- lib/src/main/java/cdc3/controller/Main.java
+++ lib/src/main/java/cdc3/controller/Main.java
@@ -2,8 +2,8 @@ package cdc3.controller;
 
 import cdc3.entities.Coche;
 import cdc3.model.JPACocheDao;
-/* Nombre:
- * UVUS:  */
+/* Nombre: Angel Donet Granado
+ * UVUS: angdongra */
 public class Main {
 
 	public static void main(String[] args) {
